d42ae606e5c50420843940a2c7d099eefbd7cda4
AaronWalker96 <aaron.walker@accenture.com>
2018-06-14 17:25:16 +0100

    updated styling for hidden contact menu

--- js/buttons.js
+++ js/buttons.js
@@ -5,3 +5,12 @@ function experimentalButton() {
 function logoButton() {
     window.location.href = "index.html";
 }
+
+function toggleVisible(divId) {
+    var contactDiv = document.getElementById(divId);
+    if (contactDiv.style.display === "none") {
+        contactDiv.style.display = "block";
+    } else {
+        contactDiv.style.display = "none";
+    }
+}
